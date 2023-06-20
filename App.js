2c2591245045480e54fdd3dd055767dd8c55ac0d
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-06-20 22:25:36 +0530

    added txt file assignment_01

--- App.js
+++ App.js
@@ -4,12 +4,17 @@
 //   "hello world from React!"
 // );
 const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement("div",{id: "child"},
-    React.createElement("h1", {}, "i am an html tag!")
-    )
-)
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am an h1 tag!"),
+    React.createElement("h2", {}, "i am an h2  tag!"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am an h1 tag!"),
+    React.createElement("h2", {}, "i am an h2  tag!"),
+  ])
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
