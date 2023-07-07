53cc9db05ef2e5a71b261c210d74050ea1b3a33a
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-07-07 16:47:37 +0530

    added assignment_02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
@@ -7,7 +9,7 @@ const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am an h1 tag!"),
+    React.createElement("h1", {}, "This is Namaste React🚀"),
     React.createElement("h2", {}, "i am an h2  tag!"),
   ]),
   React.createElement("div", { id: "child" }, [
