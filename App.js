1d548f34ab67aa862961ca729ffffd6de3455216
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-07-26 21:24:38 +0530

    add component and some css

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello world from React!"
-// );
+import Header from "./Header";
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: "heading" },
+// //   "hello world from React!"
+// // );
+//React createElement
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React🚀"),
+   [
+    React.createElement("h1", {}, "This is Namaste React -- createElement🚀"),
     React.createElement("h2", {}, "i am an h2  tag!"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am an h1 tag!"),
-    React.createElement("h2", {}, "i am an h2  tag!"),
-  ])
+    React.createElement("h3", {}, "i am an h3  tag!"),
+  ]
 );
+//React element
+// const parent1 = (
+//   <div className="title">
+//   <h1>This is Namaste React🚀</h1>
+//   <h2>i am an h2  tag!</h2>
+//   <h3>i am an h3  tag ojhal!</h3>
+//   </div>
+// )
+//react functional component
+const Parent2 = () => {
+  return (
+  <div className="title" id="container"> 
+ <h1>This is Namaste React🚀</h1>
+  <h2>i am an h2  tag!</h2>
+ <h3>i am an h3  tag ojhal123!</h3>
+  </div>
+  )
+ 
+}
+//React element
+const parent1 = (
+  <div className="title">
+    {<Parent2/>}
+    {<Header/>}
+  {/* <h1>This is Namaste React🚀</h1>
+  <h2>i am an h2  tag!</h2>
+  <h3>i am an h3  tag ojhal!</h3> */}
+  </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(parent1);
---
+++ Header.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={headerStyle}>
+      <div style={logoStyle}>
+        {/* Replace 'logo.png' with the path to your logo image */}
+        <img src="logo.png" alt="Logo" style={logoImageStyle} />
+      </div>
+      <div style={searchBarStyle}>
+        {/* You can use any search input component library or create your own */}
+        <input type="text" placeholder="Search..." style={searchInputStyle} />
+        {/* You can add a search button here if you want */}
+      </div>
+      <div style={userIconStyle}>
+        {/* Placeholder for the user icon */}
+        <img src="Sample_User_Icon.png" alt="User" style={userIconImageStyle} />
+      </div>
+    </header>
+  );
+};
+
+// Styling for the header and its components
+const headerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: '80px',
+  padding: '0 20px',
+  backgroundColor: '#f0f0f0',
+};
+
+const logoStyle = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const logoImageStyle = {
+  height: '60px', // Set the height of your logo image
+};
+
+const searchBarStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  flex: 1,
+  marginLeft: '20px', // Add some spacing between logo and search bar
+};
+
+const searchInputStyle = {
+  padding: '8px',
+  border: '1px solid #ccc',
+  borderRadius: '4px',
+  width: '100%',
+};
+
+const userIconStyle = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const userIconImageStyle = {
+  height: '40px',
+  marginLeft: '20px', // Add some spacing between search bar and user icon
+};
+
+export default Header;
