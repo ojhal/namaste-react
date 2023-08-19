986af77657186119f5e0d2e2e1f7702526d51750
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-08-19 23:53:43 +0530

    didn't use proper dynamic data yet

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Header from "./Header";
+// import Header from "./Header";
 // // const heading = React.createElement(
 // //   "h1",
 // //   { id: "heading" },
 // //   "hello world from React!"
 // // );
 //React createElement
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-   [
-    React.createElement("h1", {}, "This is Namaste React -- createElement🚀"),
-    React.createElement("h2", {}, "i am an h2  tag!"),
-    React.createElement("h3", {}, "i am an h3  tag!"),
-  ]
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//    [
+//     React.createElement("h1", {}, "This is Namaste React -- createElement🚀"),
+//     React.createElement("h2", {}, "i am an h2  tag!"),
+//     React.createElement("h3", {}, "i am an h3  tag!"),
+//   ]
+// );
 //React element
 // const parent1 = (
 //   <div className="title">
@@ -25,26 +25,82 @@ const parent = React.createElement(
 //   </div>
 // )
 //react functional component
-const Parent2 = () => {
-  return (
-  <div className="title" id="container"> 
- <h1>This is Namaste React🚀</h1>
-  <h2>i am an h2  tag!</h2>
- <h3>i am an h3  tag ojhal123!</h3>
-  </div>
-  )
+// const Parent2 = () => {
+//   return (
+//   <div className="title" id="container"> 
+//  <h1>This is Namaste React🚀</h1>
+//   <h2>i am an h2  tag!</h2>
+//  <h3>i am an h3  tag ojhal123!</h3>
+//   </div>
+//   )
  
+// }
+// //React element
+// const parent1 = (
+//   <div className="title">
+//     {<Parent2/>}
+//     {<Header/>}
+//   {/* <h1>This is Namaste React🚀</h1>
+//   <h2>i am an h2  tag!</h2>
+//   <h3>i am an h3  tag ojhal!</h3> */}
+//   </div>
+// )
+const Header = () => {
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://img.freepik.com/premium-vector/food-logo-design_139869-254.jpg?w=2000"/>
+      </div>
+      <div className="navbar">
+<ul>
+  <li>Home</li>
+  <li>About Us</li>
+  <li>Contact Us</li>
+  <li>Cart</li>
+</ul>
+      </div>
+
+    </div>
+  )
 }
-//React element
-const parent1 = (
-  <div className="title">
-    {<Parent2/>}
-    {<Header/>}
-  {/* <h1>This is Namaste React🚀</h1>
-  <h2>i am an h2  tag!</h2>
-  <h3>i am an h3  tag ojhal!</h3> */}
-  </div>
-)
+// const styleCard = {
+//   backgroundColor:"#f0f0f0"
+// }
+const RestaurantCard = ({resName}) => {
+  return(
+    <div className="res-card" style={{ backgroundColor:"#f0f0f0"}}>
+      <img className="res-logo" alt="res-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9obUN5h4xGm_YVWengZOeD9-w7QroMTyd88lJEXu_KWgIaLOrGdNHeoYfeCacrRUAK7o&usqp=CAU" />
+    <h3>{resName}</h3>
+    </div>
+  )
+}
+const Body = () =>{
+  return(
+    <div className="body">
+      <div className="search">
+        Search
+      </div>
+      <div className="res-container">
+      <RestaurantCard resName="Food Bhandar"/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      
+      </div>
+    </div>
+  )
+}
+const AppLayout = () =>{
+  return (
+    <div className="app">
+      <Header/>
+      <Body/>
+      </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent1);
+root.render(<AppLayout/>);
--- Header.js
+++ Header.js
@@ -1,66 +1,66 @@
-import React from 'react';
+// import React from 'react';
 
-const Header = () => {
-  return (
-    <header style={headerStyle}>
-      <div style={logoStyle}>
-        {/* Replace 'logo.png' with the path to your logo image */}
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiJ7CBZpGO0w58fuzfGwZf0sPkfK-KIAI_56W5-SKN&s" alt="Logo" style={logoImageStyle} />
-      </div>
-      <div style={searchBarStyle}>
-        {/* You can use any search input component library or create your own */}
-        <input type="text" placeholder="Search..." style={searchInputStyle} />
-        {/* You can add a search button here if you want */}
-      </div>
-      <div style={userIconStyle}>
-        {/* Placeholder for the user icon */}
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="User" style={userIconImageStyle} />
-      </div>
-    </header>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header style={headerStyle}>
+//       <div style={logoStyle}>
+//         {/* Replace 'logo.png' with the path to your logo image */}
+//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiJ7CBZpGO0w58fuzfGwZf0sPkfK-KIAI_56W5-SKN&s" alt="Logo" style={logoImageStyle} />
+//       </div>
+//       <div style={searchBarStyle}>
+//         {/* You can use any search input component library or create your own */}
+//         <input type="text" placeholder="Search..." style={searchInputStyle} />
+//         {/* You can add a search button here if you want */}
+//       </div>
+//       <div style={userIconStyle}>
+//         {/* Placeholder for the user icon */}
+//         <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="User" style={userIconImageStyle} />
+//       </div>
+//     </header>
+//   );
+// };
 
-// Styling for the header and its components
-const headerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  height: '80px',
-  padding: '0 20px',
-  backgroundColor: '#f0f0f0',
-};
+// // Styling for the header and its components
+// const headerStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'space-between',
+//   height: '80px',
+//   padding: '0 20px',
+//   backgroundColor: '#f0f0f0',
+// };
 
-const logoStyle = {
-  display: 'flex',
-  alignItems: 'center',
-};
+// const logoStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+// };
 
-const logoImageStyle = {
-  height: '60px', // Set the height of your logo image
-};
+// const logoImageStyle = {
+//   height: '60px', // Set the height of your logo image
+// };
 
-const searchBarStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  flex: 1,
-  marginLeft: '20px', // Add some spacing between logo and search bar
-};
+// const searchBarStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+//   flex: 1,
+//   marginLeft: '20px', // Add some spacing between logo and search bar
+// };
 
-const searchInputStyle = {
-  padding: '8px',
-  border: '1px solid #ccc',
-  borderRadius: '4px',
-  width: '100%',
-};
+// const searchInputStyle = {
+//   padding: '8px',
+//   border: '1px solid #ccc',
+//   borderRadius: '4px',
+//   width: '100%',
+// };
 
-const userIconStyle = {
-  display: 'flex',
-  alignItems: 'center',
-};
+// const userIconStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+// };
 
-const userIconImageStyle = {
-  height: '40px',
-  marginLeft: '20px', // Add some spacing between search bar and user icon
-};
+// const userIconImageStyle = {
+//   height: '40px',
+//   marginLeft: '20px', // Add some spacing between search bar and user icon
+// };
 
-export default Header;
+// export default Header;
